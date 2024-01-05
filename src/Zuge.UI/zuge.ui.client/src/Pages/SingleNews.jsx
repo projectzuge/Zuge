5cc696c54d6e5928638081421a9628be222af9c8
aleksandrababenko91 <127403692+aleksandrababenko91@users.noreply.github.com>
2024-01-05 12:44:05 +0200

    update SingleNewsPage (with datePicker and style)

--- src/Zuge.UI/zuge.ui.client/src/Pages/SingleNews.jsx
+++ src/Zuge.UI/zuge.ui.client/src/Pages/SingleNews.jsx
@@ -1,13 +1,19 @@
-import "../Styles/FrontPage.css";
 import Grid from "@mui/system/Unstable_Grid";
 import Box from "@mui/system/Box";
-import './../Styles/FrontPage.css';
 import Button from "@mui/material/Button";
 import { useEffect, useState} from "react";
 import ServiceNews from "../Components/ServiceNews";
+import { Typography } from "@mui/material";
+import "../Styles/SingleNews.css";
+import dayjs from 'dayjs';
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DateField } from '@mui/x-date-pickers/DateField';
 
 const SingleNews = () => {
   const[news, setNews] = useState([]);
+  const [value, setValue] = useState(dayjs('2024-01-05')); // when I create a News page and be able to add news; IO ll change Data format
 
   useEffect(() => {
     ServiceNews
@@ -17,27 +23,35 @@ const SingleNews = () => {
       })
   }, []);
   return (
-    <div>
-      <Button margin={10}
-          id="fetch-routes-button"
-          color={"primary"}
+    <Box>
+      <Button
+          id="takaisin-button"
+          color="primary"
           variant="contained"
       >
         Takaisin
       </Button>
-      <Box margin={10} id="search-form-container">
-         <Grid > 
-            <Grid margin={20} xs={12} sm={12} md={8} lg={6} xl={4}>
+      <Box id="single-news-container">
+         <Grid>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DemoContainer components={['DateField']}>
+            <DateField
+              value={value}
+              onChange={(newValue) => setValue(newValue)}
+            />
+          </DemoContainer>
+        </LocalizationProvider>
+        </Grid>
+            <Grid xs={12} sm={12} md={8} lg={6} xl={4}>
             {news.map((item) => (
               <div key={item.id}>
-                <h3>{item.otsikko}</h3>
-                <p>{item.teksti}</p>
+                <Typography id="header-news" >{item.otsikko}</Typography>
+                <Typography id="text-news"  align="left">{item.teksti}</Typography>
               </div>
             ))}
-            </Grid>
-          </Grid>    
+            </Grid>    
       </Box>
-    </div>
+    </Box>
   );
 };
 
