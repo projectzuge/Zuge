c62b882919dbf7716c0a6da1701af68725f153db
aleksandrababenko91 <127403692+aleksandrababenko91@users.noreply.github.com>
2024-01-10 14:35:47 +0200

    korjatan tyyli AddNewsPage

--- src/Zuge.UI/zuge.ui.client/src/Components/NewsAddForm.jsx
+++ src/Zuge.UI/zuge.ui.client/src/Components/NewsAddForm.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
+import { Grid } from "@mui/material";
 import Box from "@mui/material/Box";
 import FormControl from "@mui/material/FormControl";
-import FormLabel from "@mui/material/FormLabel";
 import TextField from "@mui/material/TextField";
 import FormGroup from "@mui/material/FormGroup";
 import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "../Theme.jsx";
 import '../Styles/NewsAddForm.css';
@@ -12,37 +13,55 @@ import '../Styles/NewsAddForm.css';
 const NewsAddForm = () => {
   
   return (
-    <Box id="search-form-container">
-      <FormGroup>
+    <Grid container>
+      <Grid item id="button-group-container">
+      <ButtonGroup   style={{ height: '70px'}} fullWidth variant="contained" aria-label="outlined primary button group">
+        <Button>Matkat</Button>
+        <Button>Käyttäjät</Button>
+        <Button>Tiedotteet</Button>  
+      </ButtonGroup>
+     
+     </Grid>
+      <Grid id="news-add-container"> 
+        <FormGroup>
         <FormControl >
-          <FormLabel>Otsikko</FormLabel>
-            <div id="single-select-div">
-              <TextField
-                id="outlined-textarea"
-                multiline
-              />            
-            </div>
-          <FormLabel>Lisää teksti</FormLabel>
-            <div id="single-select-div"> 
-              <TextField
-                id="outlined-multiline-static"
-                multiline
-                rows={4}
-                fullWidth
-              />
-            </div>
+            <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+              <Box p={1}>
+                <TextField
+                  id="outlined-multiline-static"
+                  multiline
+                  rows={1}
+                  placeholder="Otsikko"
+                  fullWidth
+
+                  />
+              </Box>
+            </Grid>
+            <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+              <Box p={1}>
+                <TextField
+                  id="outlined-multiline-static"
+                  multiline
+                  rows={10}
+                  placeholder="Lisää teksti"
+                  fullWidth
+                  />
+              </Box>
+            </Grid>
         </FormControl>
         <ThemeProvider theme={theme}>
           <Button
             color={"primary"}
             id="fetch-routes-button"
             variant="contained"
-          >
+            >
             Talenna
           </Button>
         </ThemeProvider>
-      </FormGroup>
-    </Box>
+       </FormGroup>
+      </Grid>
+    
+</Grid> 
   );
 };
 
--- src/Zuge.UI/zuge.ui.client/src/Pages/NewsPage.jsx
+++ src/Zuge.UI/zuge.ui.client/src/Pages/NewsPage.jsx
@@ -1,19 +1,12 @@
 import "../Styles/FrontPage.css";
 import NewsAddForm from "../Components/NewsAddForm";"../Components/NewsAddForm";
-import Grid from "@mui/system/Unstable_Grid";
-import Box from "@mui/system/Box";
+
 import './../Styles/FrontPage.css';
 
 function NewsPage() {
   return (
     <>
-      <Box>
-        <Grid container spacing={10}>
-          <Grid xs={12} sm={12} md={8} lg={6} xl={4}>
-            <NewsAddForm />
-          </Grid>
-        </Grid>
-      </Box>
+      <NewsAddForm />
     </>
   );
 }
