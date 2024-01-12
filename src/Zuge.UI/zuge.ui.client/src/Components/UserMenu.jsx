5e1012435c36bf6f7d913a0309dfc3879a9c477a
TomToom91 <tom9tom1@outlook.com>
2024-01-12 15:55:55 +0200

    Input validation and checks in login and register page

--- src/Zuge.UI/zuge.ui.client/src/Components/UserMenu.jsx
+++ src/Zuge.UI/zuge.ui.client/src/Components/UserMenu.jsx
@@ -1,5 +1,5 @@
 import './../Styles/UserMenu.css';
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import { Visibility, VisibilityOff } from "@mui/icons-material";
@@ -24,9 +24,109 @@ UserMenu.propTypes = {
 function UserMenu({ anchorEl, open, handleClose, handleItemClick }) {
 
   const [showPasswordUser, setShowPasswordUser] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isEmailValid, setIsEmailValid] = useState(true);
+  const [isPasswordValid, setIsPasswordValid] = useState(true);
+  const emailData = useRef(null);
+  const passwordData = useRef(null);
+  const validEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+
+  const checkPassword = () => {
+    const acceptedSmallLetters = "abcdefghijklmnopqrstuvwxyzåäö";
+    const acceptedCapitalLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖ";
+    const acceptedNumbers = "1234567890";
+    const acceptedSpecialChars = "!?-_#@{}()[]";
+    const isChars = {isSmallLetters: 0, isCapitalLetters: 0, isNumbers: 0, isSpecialChars: 0};
+
+    for (const char of password) {
+      if (acceptedSmallLetters.includes(char)) {
+        isChars.isSmallLetters = 1;
+      }
+      else if ( acceptedCapitalLetters.includes(char)) {
+        isChars.isCapitalLetters = 1;
+      }
+      else if ( acceptedNumbers.includes(char)) {
+        isChars.isNumbers = 1;
+      }
+      else if ( acceptedSpecialChars.includes(char)) {
+        isChars.isSpecialChars = 1;
+      }
+    }
+
+    if (password.length < 6 || password.length > 100) {
+      setIsPasswordValid(false);
+    }
+    else  if (!Object.values(isChars).every(value => value === 1)){
+      setIsPasswordValid(false);
+    }
+    else {
+      setIsPasswordValid(true);
+    }
+  }
+
+  const checkEmail = (e) => {
+    if (e.target?.value && e.target.value.match(validEmail)) {
+      setIsEmailValid(true);
+    }
+    else {
+      setIsEmailValid(false);
+    }
+  }
 
   const handleUserPasswordVisibility = () => {
     setShowPasswordUser(!showPasswordUser);
+  };
+
+  const onEmailChange = (e) => {
+    setEmail(emailData.current.value);
+    checkEmail(e);
+  }
+
+  const onPasswordChange = (e) => {
+    // e.preventDefault();
+    setPassword(passwordData.current.value);
+    checkPassword();
+    
+  }
+
+  const handleSignInClicked = (e) => {
+    e.preventDefault();
+    checkPassword();
+    // else {
+    //     fetch("http://localhost:3000/edit", { method: "PUT", body: data }) // "Content-Type": "application/json"
+    //     .then(res => {
+    //         if (!res.ok) {
+    //             throw new Error("Server responded " + res.status);
+    //         }
+    //         return res.json();
+    //     })
+    //     .then((data) => {
+    //         const newData = unitsData.map(unit => {
+    //             if (unit.unitName === data.unit.unitName) {
+    //                 return data.unit;
+    //             }
+    //             else {
+    //                 return unit;
+    //             }
+    //         });
+    //         setUnitsData(newData);
+    //         setSuccessPopUp(true);
+    //     })
+    //     .catch(error => {
+    //         console.log(error);
+    //     });
+    // }
+  }
+
+  const handlePasswordInput = (event) => {
+    const sanitizedValue = event.target.value.replace(/[^A-Za-z0-9!?#@\-_{(\[\])}äöåÄÖÅ]/g, '');
+    event.target.value = sanitizedValue;
+  };
+
+  const handleEmailInput = (event) => {
+    const sanitizedValue = event.target.value.replace(/[^A-Za-z0-9.@_]/g, '');
+    event.target.value = sanitizedValue;
   };
 
   const menuStyle = {
@@ -104,14 +204,29 @@ function UserMenu({ anchorEl, open, handleClose, handleItemClick }) {
             <div style={containerStyle}>
               <FormControl style={inputFieldStyle} variant="outlined">
                 <FormLabel style={labelStyle}>Sähköposti</FormLabel>
-                <TextField variant="outlined" fullWidth />
+                <TextField 
+                onInput={handleEmailInput}
+                onChange={onEmailChange}
+                error={!isEmailValid}
+                helperText={!isEmailValid ? "Invalid email address" : ""}
+                inputRef={emailData} 
+                type="email" 
+                name="email" 
+                variant="outlined" 
+                fullWidth
+                required />
               </FormControl>
               <FormControl style={inputFieldStyle} variant="outlined">
                 <FormLabel style={labelStyle}>Salasana</FormLabel>
                 <TextField 
+                inputRef={passwordData}
+                name="password"
                 variant="outlined" 
                 type={showPasswordUser ? 'text' : 'password'} 
                 fullWidth 
+                error={!isPasswordValid}
+                helperText={!isPasswordValid ? "Invalid password" : ""}
+                required
                 InputProps={{
                   endAdornment: (
                     <InputAdornment position="end">
@@ -121,10 +236,12 @@ function UserMenu({ anchorEl, open, handleClose, handleItemClick }) {
                     </InputAdornment>
                   ),
                 }}
+                onInput={handlePasswordInput}
+                onChange={onPasswordChange}
                 />
               </FormControl>
               <div style={buttonsContainerStyle}>
-                <Button  onClick={handleItemClick} style={buttonStyle}>
+                <Button  onClick={handleSignInClicked} style={buttonStyle}>
                   Kirjaudu
                 </Button>
                 <Button  onClick={handleItemClick} style={buttonStyle}>
--- src/Zuge.UI/zuge.ui.client/src/Pages/Register.jsx
+++ src/Zuge.UI/zuge.ui.client/src/Pages/Register.jsx
@@ -1,5 +1,5 @@
 import './../Styles/Register.css';
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { Container, TextField, Button, Grid, InputAdornment, IconButton, InputLabel } from '@mui/material';
 import { Visibility, VisibilityOff } from "@mui/icons-material";
 import { Link } from "react-router-dom";
@@ -11,15 +11,104 @@ import { Link } from "react-router-dom";
 function Register() {
     const [showPassword, setShowPassword] = useState(false);
     const [showRePassword, setShowRePassword] = useState(false);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [rePassword, setRePassword] = useState('');
+    const [isEmailValid, setIsEmailValid] = useState(true);
+    const [isPasswordValid, setIsPasswordValid] = useState(true);
+    const [isRePasswordValid, setIsRePasswordValid] = useState(true);
+    const emailData = useRef(null);
+    const passwordData = useRef(null);
+    const rePasswordData = useRef(null);
+    const validEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
 
     const labelStyle = {
         display: "grid",
         justifyContent: "start",
+        color: "#262626",
     };
 
     const buttonStyle = {
         color: '#262626',
     };
+
+    const arePasswordsEqual = () => {
+        if (password === rePassword) {
+            console.log("Passwords are the same!!!!");
+        }
+        else {
+            console.log("Passwords are not the same!!!");
+        }
+    }
+
+    const checkPassword = (passw) => {
+        const acceptedSmallLetters = "abcdefghijklmnopqrstuvwxyzåäö";
+        const acceptedCapitalLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖ";
+        const acceptedNumbers = "1234567890";
+        const acceptedSpecialChars = "!?-_#@{}()[]";
+        const isChars = {isSmallLetters: 0, isCapitalLetters: 0, isNumbers: 0, isSpecialChars: 0};
+    
+        for (const char of passw) {
+          if (acceptedSmallLetters.includes(char)) {
+            isChars.isSmallLetters = 1;
+          }
+          else if ( acceptedCapitalLetters.includes(char)) {
+            isChars.isCapitalLetters = 1;
+          }
+          else if ( acceptedNumbers.includes(char)) {
+            isChars.isNumbers = 1;
+          }
+          else if ( acceptedSpecialChars.includes(char)) {
+            isChars.isSpecialChars = 1;
+          }
+        }
+    
+        if (passw.length < 6 || passw.length > 100) {
+            if (password === rePassword) {
+                setIsPasswordValid(false);
+                setIsRePasswordValid(false);
+            }
+            else if (passw === password && passw !== rePassword) {
+                setIsPasswordValid(false);
+            }
+            else {
+                setIsRePasswordValid(false);
+            }
+        }
+        else  if (!Object.values(isChars).every(value => value === 1)){
+            if (password === rePassword) {
+                setIsPasswordValid(false);
+                setIsRePasswordValid(false);
+            }
+            else if (passw === password && passw !== rePassword) {
+                setIsPasswordValid(false);
+            }
+            else {
+                setIsRePasswordValid(false);
+            }
+        }
+        else {
+            if (password === rePassword) {
+                setIsPasswordValid(true);
+                setIsRePasswordValid(true);
+            }
+            else if (passw === password && passw !== rePassword) {
+                setIsPasswordValid(true);
+            }
+            else {
+                setIsRePasswordValid(true);
+            }
+        }
+      }
+    
+      const checkEmail = (e) => {
+        if (e.target?.value && e.target.value.match(validEmail)) {
+          setIsEmailValid(true);
+        }
+        else {
+          setIsEmailValid(false);
+        }
+      }
 
     const handlePasswordVisibility = () => {
         setShowPassword(!showPassword);
@@ -27,6 +116,62 @@ function Register() {
 
     const handleRePasswordVisibility = () => {
         setShowRePassword(!showRePassword);
+    };
+
+    const onEmailChange = (e) => {
+        setEmail(emailData.current.value);
+        checkEmail(e);
+      }
+    
+    const onPasswordChange = (e) => {
+        e.preventDefault();
+        setPassword(passwordData.current.value);
+        checkPassword(password);
+    }
+
+    const onRePasswordChange = (e) => {
+        e.preventDefault();
+        setRePassword(rePasswordData.current.value);
+        checkPassword(rePassword);
+    }
+
+    const handleRegisterClicked = (e) => {
+        e.preventDefault();
+        arePasswordsEqual();
+        // else {
+        //     fetch("http://localhost:3000/edit", { method: "PUT", body: data }) // "Content-Type": "application/json"
+        //     .then(res => {
+        //         if (!res.ok) {
+        //             throw new Error("Server responded " + res.status);
+        //         }
+        //         return res.json();
+        //     })
+        //     .then((data) => {
+        //         const newData = unitsData.map(unit => {
+        //             if (unit.unitName === data.unit.unitName) {
+        //                 return data.unit;
+        //             }
+        //             else {
+        //                 return unit;
+        //             }
+        //         });
+        //         setUnitsData(newData);
+        //         setSuccessPopUp(true);
+        //     })
+        //     .catch(error => {
+        //         console.log(error);
+        //     });
+        // }
+    }
+
+    const handlePasswordInput = (event) => {
+    const sanitizedValue = event.target.value.replace(/[^A-Za-z0-9!?#@\-_{(\[\])}äöåÄÖÅ]/g, '');
+    event.target.value = sanitizedValue;
+    };
+
+    const handleEmailInput = (event) => {
+    const sanitizedValue = event.target.value.replace(/[^A-Za-z0-9.@_]/g, '');
+    event.target.value = sanitizedValue;
     };
 
   return (
@@ -49,7 +194,14 @@ function Register() {
                         <TextField 
                         className="registerTextField"
                         variant="outlined" 
-                        fullWidth />
+                        onInput={handleEmailInput}
+                        onChange={onEmailChange}
+                        error={!isEmailValid}
+                        helperText={!isEmailValid ? "Invalid email address" : ""}
+                        inputRef={emailData} 
+                        type="email" 
+                        fullWidth
+                        required />
                     </Grid>
                     <Grid item xs={6}>
                         <InputLabel 
@@ -64,9 +216,16 @@ function Register() {
                         style={labelStyle}>Salasana</InputLabel>
                         <TextField 
                         className="registerTextField"
+                        inputRef={passwordData}
+                        name="password"
                         variant="outlined" 
                         type={showPassword ? 'text' : 'password'} 
                         fullWidth 
+                        error={!isPasswordValid}
+                        helperText={!isPasswordValid ? "Invalid password" : ""}
+                        required
+                        onInput={handlePasswordInput}
+                        onChange={onPasswordChange}
                         InputProps={{
                             endAdornment: (
                             <InputAdornment position="end">
@@ -93,9 +252,16 @@ function Register() {
                         style={labelStyle}>Vahvista salasana</InputLabel>
                         <TextField  
                         className="registerTextField"
+                        inputRef={rePasswordData}
+                        name="password"
                         variant="outlined" 
                         type={showRePassword ? 'text' : 'password'}  
                         fullWidth 
+                        error={!isRePasswordValid}
+                        helperText={!isRePasswordValid ? "Invalid password" : ""}
+                        required
+                        onInput={handlePasswordInput}
+                        onChange={onRePasswordChange}
                         InputProps={{
                             endAdornment: (
                             <InputAdornment position="end">
@@ -159,6 +325,7 @@ function Register() {
                     </Grid>
                     <Grid item xs={6}>
                         <Button 
+                        onClick={handleRegisterClicked}
                         style={buttonStyle} 
                         variant="contained" 
                         color="primary" 
