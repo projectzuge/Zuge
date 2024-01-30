c800a1081da4c5cf2414ca15616cf27c23eecd97
Arttu Knuutinen <arttu.knuutinen@gmail.com>
2024-01-30 09:49:10 +0200

    Create test users for in-memory db

--- src/Zuge.UI/Zuge.UI.Server/Program.cs
+++ src/Zuge.UI/Zuge.UI.Server/Program.cs
@@ -22,7 +22,6 @@ _ = builder.Services
     .AddSingleton<IPaymentGateway, NoneGateway>();
 
 _ = builder.Services
-    //.AddDbContext<AuthenticationDbContext>(options => options.UseNpgsql("Database=postgres;Host=localhost;Password=12345;Username=postgres"))
     .AddDbContext<AuthenticationDbContext>(options => options.UseInMemoryDatabase("IdentityAuth"))
     .AddAuthorization(options =>
     {
@@ -32,6 +31,7 @@ _ = builder.Services
     })
     .AddIdentityApiEndpoints<ApplicationUser>()
     .AddRoles<IdentityRole>()
+    .AddUserManager<ApplicationUserManager>()
     .AddRoleManager<RoleManager<IdentityRole>>()
     .AddEntityFrameworkStores<AuthenticationDbContext>();
 
@@ -58,6 +58,7 @@ _ = app
 
 _ = app.MapControllers();
     
+#region map auth endpoints
 _ = app.MapGroup("/account").MapIdentityApi<ApplicationUser>();
 
 _ = app.MapPost("/account/logout", async (SignInManager<ApplicationUser> signInManager,
@@ -70,20 +71,19 @@ _ = app.MapPost("/account/logout", async (SignInManager<ApplicationUser> signInM
         }
         return Results.Unauthorized();
     }).RequireAuthorization();
-
 _ = app.MapGet("/account/pingauth/", (ClaimsPrincipal user) =>
     {
         var email = user.FindFirstValue(ClaimTypes.Email);
         var role = user.FindFirstValue(ClaimTypes.Role);
         return Results.Json(new { Email = email, Role = role });
-    }).RequireAuthorization();
+    }).RequireAuthorization("User");
 
 _ = app.MapGet("/account/pingauth/employee", (ClaimsPrincipal user) =>
 {
     var email = user.FindFirstValue(ClaimTypes.Email);
     var role = user.FindFirstValue(ClaimTypes.Role);
     return Results.Json(new { Email = email, Role = role });
-}).RequireAuthorization("Employee", "Admin");
+}).RequireAuthorization("Employee");
 
 _ = app.MapGet("/account/pingauth/admin", (ClaimsPrincipal user) =>
 {
@@ -91,6 +91,7 @@ _ = app.MapGet("/account/pingauth/admin", (ClaimsPrincipal user) =>
     var role = user.FindFirstValue(ClaimTypes.Role);
     return Results.Json(new { Email = email, Role = role });
 }).RequireAuthorization("Admin");
+#endregion
 
 _ = app.MapFallbackToFile("/index.html");
 
@@ -101,11 +102,40 @@ var journeys = JsonSerializer.Deserialize<IEnumerable<Journey>>(json) ?? [];
 domain.Repository<Journey>().AddRange(journeys);
 await domain.CommitAsync();
 
-var auth = scope.ServiceProvider.GetRequiredService<IAuthUnitOfWork>();
-var users_json = await File.ReadAllTextAsync("user_accounts.json");
-var users = JsonSerializer.Deserialize<IEnumerable<UserAccount>>(users_json) ?? [];
-auth.Repository<UserAccount>().AddRange(users);
-await auth.CommitAsync();
+#region create test users for in-memory db
+var userManager = scope.ServiceProvider.GetRequiredService<ApplicationUserManager>();
+var userStore = scope.ServiceProvider.GetRequiredService<IUserStore<ApplicationUser>>();
+var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+var emailStore = (IUserEmailStore<ApplicationUser>)userStore;
+
+string adminEmail = "admin@zuge.fi";
+string empEmail = "employee@zuge.fi";
+string userEmail = "user@zuge.fi";
+string password = "P@ssw0rd";
+
+var admin = new ApplicationUser { FirstName = "Test", LastName = "Admin", PhoneNumber = "1234567890" };
+var employee = new ApplicationUser { FirstName = "Test", LastName = "Employee", PhoneNumber = "1234567890" };
+var user = new ApplicationUser { FirstName = "Test", LastName = "User", PhoneNumber = "1234567890" };
+
+await roleManager.CreateAsync(new IdentityRole("Admin"));
+await roleManager.CreateAsync(new IdentityRole("Employee"));
+await roleManager.CreateAsync(new IdentityRole("User"));
+
+await userStore.SetUserNameAsync(admin, adminEmail, CancellationToken.None);
+await emailStore.SetEmailAsync(admin, adminEmail, CancellationToken.None);
+await userManager.CreateAsync(admin, password);
+await userManager.AddToRolesAsync(admin, ["Admin", "Employee", "User"]);
+
+await userStore.SetUserNameAsync(employee, empEmail, CancellationToken.None);
+await emailStore.SetEmailAsync(employee, empEmail, CancellationToken.None);
+await userManager.CreateAsync(employee, password);
+await userManager.AddToRolesAsync(employee, ["Employee", "User"]);
+
+await userStore.SetUserNameAsync(user, userEmail, CancellationToken.None);
+await emailStore.SetEmailAsync(user, userEmail, CancellationToken.None);
+await userManager.CreateAsync(user, password);
+await userManager.AddToRoleAsync(user, "User");
+#endregion
 
 app.Run();
 
