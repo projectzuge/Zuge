7f792a5857b5c425206e6aa7854a7649f05a5c7d
TomToom91 <tom9tom1@outlook.com>
2024-02-08 14:49:04 +0200

    More unit tests

--- src/Zuge.UI/zuge.ui.client/src/UnitTests/LoginForm.test.jsx
+++ src/Zuge.UI/zuge.ui.client/src/UnitTests/LoginForm.test.jsx
@@ -1,6 +1,8 @@
 import { expect, test, vi } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import LoginForm from './../Components/LoginForm.jsx';
+
+// RENDERING TESTS:
 
 vi.mock('react-router-dom', () => ({
     ...vi.importActual('react-router-dom'),
@@ -30,7 +32,7 @@ test('renders cancel text correctly', () => {
 });
 
 test('renders Register text correctly', () => {
-    expect(screen.getByText('Rekisteröidy'));
+    expect(screen.getByText(/\bRekisteröidy\b/i));
 });
 
 test('renders email textfield correctly', () => {
@@ -45,3 +47,179 @@ test('does not show "Sign in failed text" when first rendered', () => {
     expect(screen.queryByText('Kirjautuminen epäonnistui.')).toBeInTheDocument();
     expect(screen.queryByText('Kirjautuminen epäonnistui.')).not.toBeVisible();
 });
+
+test('does not show "Invalid email" when first rendered', async () => {
+    expect(screen.queryByText(/Invalid email address/i)).toBeNull();
+});
+
+test('does not show "Invalid password" when first rendered', () => {
+    expect(screen.queryByText(/Invalid password/i)).toBeNull();
+});
+
+// FUNCTIONALITY TESTS:
+
+const emailLocalPartAcceptable = [
+    "Tommy",
+    "tommy",
+    "tOmMy",
+    "TOMMY",
+    "tommy.niemi",
+    "tOmMy.NiEmI",
+    "Tommy.Niemi",
+    "TOMMY.NIEMI",
+    "tommy.nie.mi",
+    "tOMMy.nIE.MI",
+    "TOMMY.NIE.MI",
+    "123",
+    "1Tommy",
+    "Tomm1",
+    "tOm3my",
+    "34.tommy",
+    "tommy.5",
+    "tommy.678.niemi",
+    "389.tommy.niemi",
+    "12.tommy.13",
+    "tom.23.14",
+    "33.66.tom",
+    "tommy_12",
+    "34_tommy",
+    "tom_my",
+    "niemi_tommy",
+    "tommy.nie_mi",
+    "tommy_nie-mi",
+    "tommy-niemi",
+    "tommy_niemi",
+    "tommy_nie_mi",
+    "tommy-nie-mi",
+    "abcdefghijklmnopqrstuvwxyz",
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+    "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+    "_tommy",
+    "5tommy7",
+    "9tommy",
+    "8tommy.niemi",
+];
+
+const emailLocalPartNotAcceptable = [
+    ".tommy",
+    "!tommy",
+    "tommy!",
+    "tom?my",
+    "  tommy",
+    "tom my",
+    "tom!my.niemi",
+    "tommy .niemi",
+    "tommy.niem1",
+    "tommy.niemi ",
+    "tömmy",
+    "tommy_niömi",
+    "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+    "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+    "abcdefghijklmnopqrstuvwxyzåäöABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+    "",
+    "",
+    "",
+    ""
+];
+
+const emailDomainPartAcceptable = [
+    "gmail.com",
+    "email.com",
+    "email.org",
+    "email.co.uk",
+    "email.co",
+    "email.fi",
+    "email1.com",
+    "em-ail.fi",
+    "em4il.co.uk",
+    "EMAIL.com",
+    "Email.fi",
+    "eMail.org",
+    "em-M4IL.com",
+    "-GMAIL.com",
+    "em_ail.fi",
+    "E_MAIL.co",
+];
+
+const emailDomainPartNotAcceptable = [
+    ".email.com",
+    "ema!l.com",
+    ".gmail.com",
+    "a",
+    ".com",
+    ".fi",
+    "",
+    "",
+    "",
+    "",
+    ""
+];
+
+const passwordsAcceptable = [
+    "aE1!22",
+    "abC123!?",
+    "OOOooo123!!!$$$",
+    "£££eeeEEE122",
+    "536rT@@",
+    "aaaaaaaaaaaaaaabbbbbbbbbbbbeeeeeeeeeeeeeeeeeCCCCCCCCCCCCCCCCCCCC123456789!?????????????????????????",
+    "abcdefghijklmnopqrstuvwxyzåäöABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖ1234567890!?@£$#.-_",
+    " kjkdj12!?JJ",
+];
+
+const passwordsNotAcceptable = [
+    "aE1!2",
+    "lllooo123!!!$$$",
+    "£££eeeEEE&&&",
+    "536rt@@",
+    "aaaaaaaaaaaaaaabbbbbbbbbbbbeeeeeeeeeeeeeeeeeCCCCCCCCCCCCCCCCCCCC123456789!???????????????????????????",
+    "aaaaaaaaaaaaaaabbbbbbbbbbbbeeeeeeeeeeeeeeeeeCCCCCCCCCCCCCCCCCCCC123456789!???????????????????????????aaaaaaaaaaaaaaabbbbbbbbbbbbeeeeeeeeeeeeeeeeeCCCCCCCCCCCCCCCCCCCC123456789!???????????????????????????",
+]
+
+for (let i = 0; i < 50; i++) {
+    const password = passwordsAcceptable[Math.floor(Math.random() * passwordsAcceptable.length)];
+
+    test('password field accepts password: ' + password, () => {
+        const passwordField = screen.queryByLabelText('Salasana');
+        fireEvent.change(passwordField, { target: { value: password } });
+
+        expect(screen.queryByText(/Invalid password/i)).toBeNull();
+    });
+}
+
+for (let i = 0; i < 50; i++) {
+    const email = emailLocalPartAcceptable[Math.floor(Math.random() * emailLocalPartAcceptable.length)] + "@" +
+     emailDomainPartAcceptable[Math.floor(Math.random() * emailDomainPartAcceptable.length)];
+
+    test('email field accepts email: ' + email, () => {
+        const emailField = screen.getByRole('textbox', { type: "email" });
+        fireEvent.change(emailField, { target: { value: email } });
+
+        expect(screen.queryByText(/Invalid email address/i)).toBeNull();
+
+    });
+}
+
+for (let i = 0; i < 50; i++) {
+    const password = passwordsNotAcceptable[Math.floor(Math.random() * passwordsNotAcceptable.length)];
+
+    test('password field does not accept password: ' + password, () => {
+        const passwordField = screen.getByRole("textbox", {name: /Salasana/i});
+        fireEvent.change(passwordField, { target: { value: password } });
+        // console.log(screen.queryByText(/Invalid password/i));
+
+        expect(screen.queryByText(/Invalid password/i)).not.toBeNull();
+    });
+}
+
+for (let i = 0; i < 50; i++) {
+    const email = emailLocalPartNotAcceptable[Math.floor(Math.random() * emailLocalPartNotAcceptable.length)] + "@" +
+     emailDomainPartNotAcceptable[Math.floor(Math.random() * emailDomainPartNotAcceptable.length)];
+
+    test('email field does not accept email: ' + email, () => {
+        const emailField = screen.getByRole('textbox', { type: "email" });
+        fireEvent.change(emailField, { target: { value: email } });
+
+        expect(screen.queryByText(/Invalid email address/i)).not.toBeNull();
+
+    });
+}
