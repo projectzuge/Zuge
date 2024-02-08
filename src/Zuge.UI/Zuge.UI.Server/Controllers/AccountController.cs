32211a8d00b5c3463f377ad3ed75cb40a3bc013e
Arttu Knuutinen <arttu.knuutinen@gmail.com>
2024-02-08 10:40:03 +0200

    Return user data on PUT

--- src/Zuge.UI/Zuge.UI.Server/Controllers/AccountController.cs
+++ src/Zuge.UI/Zuge.UI.Server/Controllers/AccountController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.ComponentModel.DataAnnotations;
+using System.Data;
 using Zuge.Infrastructure;
 
 namespace Zuge.UI.Server.Controllers;
@@ -19,70 +21,24 @@ public class AccountController(AuthenticationDbContext authenticationDbContext, 
     public async Task<IActionResult> OnGetAsync()
     {
         var appUser = await userManager.GetUserAsync(User);
+        if (appUser == null) return NotFound("This should not happen");
+        var roles = await userManager.GetRolesAsync(appUser);
+        var userId = await userManager.GetUserIdAsync(appUser);
 
-        [HttpGet]
-        [Authorize(Policy = "User")]
-        public async Task<IActionResult> OnGetAsync()
-        {
-            var appUser = await _userManager.GetUserAsync(User);
-            if (appUser == null) return NotFound("This should not happen");
-            var roles = await _userManager.GetRolesAsync(appUser);
-
-            return Ok(new { appUser.Email, appUser.FirstName, appUser.LastName, appUser.PhoneNumber, roles });
-        }
-        [HttpGet]
-        [Route("authorize")]
-        [Authorize(Policy = "User")]
-        public async Task<IActionResult> OnGetAuthAsync()
-        {
-            var appUser = await _userManager.GetUserAsync(User);
-            if (appUser == null) return NotFound("This should not happen");
-            var roles = await _userManager.GetRolesAsync(appUser);
-            var userId = await _userManager.GetUserIdAsync(appUser);
-            return Ok(new { roles, userId });
-        }
-
-        [HttpPost]
-        [Route("manage/register")]
-        public async Task<IActionResult> OnPostAsync([FromBody] RegistrationInformation info, [FromQuery] string email)
-        {
-            var user = _authenticationDbContext.Users.FirstOrDefault(user => user.Email == email);
-            if (user == null) return NotFound();
-
-            var trimmedInfo = new RegistrationInformation(
-                info.FirstName.Trim(),
-                info.LastName.Trim(),
-                info.PhoneNumber?.Trim());
-
-            if (!TryValidateModel(trimmedInfo)) return BadRequest();
-            
-            user.FirstName = trimmedInfo.FirstName;
-            user.LastName = trimmedInfo.LastName;
-            user.PhoneNumber = trimmedInfo.PhoneNumber;
-            await _userManager.AddToRoleAsync(user, "User");
-            await _userManager.UpdateAsync(user);
-
-            return Ok();
-        }
-        [HttpPut]
-        [Route("manage/info")]
-        [Authorize(Policy = "User")]
-        public async Task<IActionResult> OnPutAsync([FromBody] RegistrationInformation info)
-        {
-            if (!TryValidateModel(info)) return BadRequest();
-
-            var user = await _userManager.GetUserAsync(User);
-            if (user == null) return NotFound();
-
-            user.FirstName = info.FirstName;
-            user.LastName = info.LastName;
-            user.PhoneNumber = info.PhoneNumber;
-            await _userManager.UpdateAsync(user);
-
-            return Ok();
-        }
+        return Ok(new { appUser.Email, appUser.FirstName, appUser.LastName, appUser.PhoneNumber, roles, userId });
     }
 
+    [HttpGet]
+    [Route("authorize")]
+    [Authorize(Policy = "User")]
+    public async Task<IActionResult> OnGetAuthAsync()
+    {
+        var appUser = await userManager.GetUserAsync(User);
+        if (appUser == null) return NotFound("This should not happen");
+        var roles = await userManager.GetRolesAsync(appUser);
+        var userId = await userManager.GetUserIdAsync(appUser);
+        return Ok(new { roles, userId });
+    }
 
     [HttpPost]
     [Route("manage/register")]
@@ -120,7 +76,9 @@ public class AccountController(AuthenticationDbContext authenticationDbContext, 
         user.LastName = info.LastName;
         user.PhoneNumber = info.PhoneNumber;
         await userManager.UpdateAsync(user);
+        var roles = await userManager.GetRolesAsync(user);
+        var userId = await userManager.GetUserIdAsync(user);
 
-        return Ok();
+        return Ok(new { user.Email, user.FirstName, user.LastName, user.PhoneNumber, roles, userId });
     }
 }
