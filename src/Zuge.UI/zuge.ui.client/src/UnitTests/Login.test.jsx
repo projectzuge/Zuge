4524854ac102816f336dfd4af0811b049386bd63
TomToom91 <tom9tom1@outlook.com>
2024-02-12 15:42:34 +0200

    Cleaned and refactored components

--- src/Zuge.UI/zuge.ui.client/src/UnitTests/Login.test.jsx
+++ src/Zuge.UI/zuge.ui.client/src/UnitTests/Login.test.jsx
@@ -1,6 +1,12 @@
 import { expect, test, vi } from 'vitest';
 import { render, screen, fireEvent } from '@testing-library/react';
-import LoginForm from './../Components/LoginForm.jsx';
+import Login from './../Components/Login.jsx';
+import { 
+    emailLocalPartAcceptable,
+    emailLocalPartNotAcceptable,
+    emailDomainPartAcceptable,
+    emailDomainPartNotAcceptable,
+} from "./../assets/EmailsPasswords.jsx";
 
 // RENDERING TESTS:
 
@@ -11,7 +17,7 @@ vi.mock('react-router-dom', () => ({
 }));
 
 test('renders password text correctly', () => {
-    render(<LoginForm />);
+    render(<Login />);
     expect(screen.getByText('Salasana'));
 });
 
@@ -57,135 +63,6 @@ test('does not show "Invalid password" when first rendered', () => {
 });
 
 // FUNCTIONALITY TESTS:
-
-const emailLocalPartAcceptable = [
-    "Tommy",
-    "tommy",
-    "tOmMy",
-    "TOMMY",
-    "tommy.niemi",
-    "tOmMy.NiEmI",
-    "Tommy.Niemi",
-    "TOMMY.NIEMI",
-    "tommy.nie.mi",
-    "tOMMy.nIE.MI",
-    "TOMMY.NIE.MI",
-    "123",
-    "1Tommy",
-    "Tomm1",
-    "tOm3my",
-    "34.tommy",
-    "tommy.5",
-    "tommy.678.niemi",
-    "389.tommy.niemi",
-    "12.tommy.13",
-    "tom.23.14",
-    "33.66.tom",
-    "tommy_12",
-    "34_tommy",
-    "tom_my",
-    "niemi_tommy",
-    "tommy.nie_mi",
-    "tommy_nie-mi",
-    "tommy-niemi",
-    "tommy_niemi",
-    "tommy_nie_mi",
-    "tommy-nie-mi",
-    "abcdefghijklmnopqrstuvwxyz",
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
-    "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789." +
-    "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
-    "_tommy",
-    "5tommy7",
-    "9tommy",
-    "8tommy.niemi",
-    "tommy.niem1",
-    "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789." +
-    "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
-    "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789." +
-    "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789." +
-    "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
-    ".tommy",
-    "  tommy",
-];
-
-const emailLocalPartNotAcceptable = [
-    "!tommy",
-    "tom?my",
-    "tom my",
-    "tom!my.niemi",
-    "tommy!",
-    "tommy .niemi",
-    "tommy.niemi ",
-    "tömmy",
-    "tommy_niömi",
-    "abcdefghijklmnopqrstuvwxyzåäöABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
-    "",
-    "",
-    "",
-    ""
-];
-
-const emailDomainPartAcceptable = [
-    "gmail.com",
-    "email.com",
-    "email.org",
-    "email.co.uk",
-    "email.co",
-    "email.fi",
-    "email1.com",
-    "em-ail.fi",
-    "em4il.co.uk",
-    "EMAIL.com",
-    "Email.fi",
-    "eMail.org",
-    "em-M4IL.com",
-    "-GMAIL.com",
-    "em_ail.fi",
-    "E_MAIL.co",
-];
-
-const emailDomainPartNotAcceptable = [
-    ".email.com",
-    "ema!l.com",
-    ".gmail.com",
-    "a",
-    ".com",
-    ".fi",
-    "",
-    "",
-    "",
-    "",
-    ""
-];
-
-// const passwordsAcceptable = [
-//     "aE1!22",
-//     "abC123!?",
-//     "OOOooo123!!!$$$",
-//     "£££eeeEEE122",
-//     "536rT@@",
-//     "aaaaaaaaaaaaaaabbbbbbbbbbbbeeeeeeeeeeeeeeeeeCCCCCCCCCCCCCCCCCCCC" +
-//     "123456789!?????????????????????????",
-//     "abcdefghijklmnopqrstuvwxyzåäöABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖ" +
-//     "1234567890!?@£$#.-_",
-//     " kjkdj12!?JJ",
-//     "aB/c12!?"
-// ];
-
-// const passwordsNotAcceptable = [
-//     "aE1!2",
-//     "lllooo123!!!$$$",
-//     "£££eeeEEE&&&",
-//     "536rt@@",
-//     "aaaBBB123",
-//     "aaaaaaaaaaaaaaabbbbbbbbbbbbeeeeeeeeeeeeeeeeeCCCCCCCCCCCCCCCCCCCC" +
-//     "123456789!???????????????????????????",
-//     "aaaaaaaaaaaaaaabbbbbbbbbbbbeeeeeeeeeeeeeeeeeCCCCCCCCCCCCCCCCCCCC" +
-//     "123456789!???????????????????????????aaaaaaaaaaaaaaabbbbbbbbbbbb" +
-//     "eeeeeeeeeeeeeeeeeCCCCCCCCCCCCCCCCCCCC123456789" +
-//     "!???????????????????????????",
-// ]
 
 for (let i = 0; i < 50; i++) {
     const email = emailLocalPartAcceptable[Math.floor(Math.random() * 
