805c05d15e8754f2b8c8f8580dadc733360b820a
Saara Sairanen <s.sairanen@hotmail.com>
2024-02-14 12:58:19 +0200

    remove useEffect

--- src/Zuge.UI/zuge.ui.client/src/Pages/RegisterSuccess.jsx
+++ src/Zuge.UI/zuge.ui.client/src/Pages/RegisterSuccess.jsx
@@ -1,50 +1,56 @@
 import "./../Styles/RegisterSuccess.css";
 import { Typography, Button } from "@mui/material";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import { toast } from "react-toastify";
-import { useNavigate } from 'react-router-dom';
-import { useRef, useEffect } from 'react';
+import { useNavigate } from "react-router-dom";
+import { useRef, useEffect } from "react";
 
 RegisterSuccess.propTypes = {
   DarkMode: PropTypes.bool,
 };
 
-function RegisterSuccess({DarkMode}) {
-  useEffect(() => {
-    window.scrollTo(0, 0);
-    toast.dismiss();
-  }, []);
+function RegisterSuccess({ DarkMode }) {
+  toast.dismiss();
+
   const navigate = useNavigate();
   const targetElementRef = useRef(null);
 
   useEffect(() => {
     console.log(targetElementRef);
     if (targetElementRef.current) {
-      targetElementRef.current.scrollIntoView({ behavior: 'smooth', block: 'center' });
+      targetElementRef.current.scrollIntoView({
+        behavior: "smooth",
+        block: "center",
+      });
     }
   }, [targetElementRef]);
 
   return (
     <>
-      <div ref={targetElementRef} className={DarkMode? "RegisterSuccessBodyDark" :
-      "RegisterSuccessBodyLight"}>
-        <Typography>
-          Rekisteröityminen onnistui!
-        </Typography>
+      <div
+        ref={targetElementRef}
+        className={
+          DarkMode ? "RegisterSuccessBodyDark" : "RegisterSuccessBodyLight"
+        }
+      >
+        <Typography>Rekisteröityminen onnistui!</Typography>
         <Button
-        sx={{
-          padding: "10px",
-          margin: "20px",
-          borderStyle: "solid",
-          borderWidth: "1px",
-          borderColor: DarkMode? "#eeeeee" : "#262626",
-        }}
-        onClick={() => {navigate("/")}}
+          sx={{
+            padding: "10px",
+            margin: "20px",
+            borderStyle: "solid",
+            borderWidth: "1px",
+            borderColor: DarkMode ? "#eeeeee" : "#262626",
+          }}
+          onClick={() => {
+            navigate("/");
+          }}
         >
-          Etusivulle</Button>
+          Etusivulle
+        </Button>
       </div>
     </>
   );
 }
 
-  export default RegisterSuccess;
+export default RegisterSuccess;
